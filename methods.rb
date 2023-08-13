567a2a870f3a99918071a5b2b572cdda56848e8a
Ray Zana <rayzana280@gmail.com>
2023-08-13 12:14:37 -0400

    commit tree a2bf107a22289e6ec1cc11b78a88ff40f27e6363

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+#function greetProgrammer() {
+# console.log("Hello, programmer!");
+#}
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end 
+
+greet "Naureen"
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end 
+
+greet_with_default
+
+def add num1 , num2
+    return num1 + num2
+end 
+
+def halve number
+    if number.class != Integer
+        return nil
+    elsif number == 0
+        return nil
+    end
+
+    return number / 2
+end  
